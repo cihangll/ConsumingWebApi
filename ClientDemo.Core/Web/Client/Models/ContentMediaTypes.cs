988284a040fb1427cedcb8b4d98aa1e0165211f9
Cihan Güllü <cihangullu54@gmail.com>
2019-06-11 17:51:53 +0300

    renamed MediaTypes - ContentMediaTypes

--- ClientDemo.Core/Web/Client/Models/ContentMediaTypes.cs
+++ ClientDemo.Core/Web/Client/Models/ContentMediaTypes.cs
@@ -1,6 +1,6 @@
 ﻿namespace ClientDemo.Core.Web.Client.Models
 {
-	public static class MediaTypes
+	public static class ContentMediaTypes
 	{
 		public const string UrlEncoded = "application/x-www-form-urlencoded";
 		public const string Json = "application/json";
